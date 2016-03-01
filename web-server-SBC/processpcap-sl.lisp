10a3262622d6010d88d59ab6d3973ac49230e18d
nukuok <hyy314159@gmail.com>
2016-03-01 09:02:12 +0900

    update

---
+++ web-server-SBC/processpcap-sl.lisp
@@ -0,0 +1,37 @@
+(in-package :sbc-tools)
+
+(defvar *processpcap-data*)
+(setf *processpcap-data* "saved_data/processpcap/db.lisp")
+
+;;test
+;;(setf *temp* '(setf *tt* (1 2 (3 4) (5 6))))
+;;(defun save-db (filename)
+;;  (with-open-file (out filename
+;;		       :direction :output
+;;		       :if-exists :supersede)
+;;    (with-standard-io-syntax (print *temp* out))))
+;;
+;;(defun load-db (filename)
+;;  (with-open-file (in filename)
+;;    (with-standard-io-syntax
+;;      (setf *db* (read in)))))
+
+(defun new-data (dataname)
+  (push (list dataname *filepath* *ip-header-list* *color-rule*)
+	      *processpcap-data*))
+
+(defun remove-data (dataname)
+  (remove dataname *processpcap-data* :test (lambda (x) (equal dataname (car x)))))
+
+(defun get-data-list ()
+  (mapcar #'car *processpcap-data*))
+
+(defun load-data (datename)
+  (let ((mame (assoc dataname *processpcap-data*)))
+    (setf *filepath* (cadr mame)
+	  *ip-header-list* (caddr mame)
+	  *color-rule* (cadddr mame))))
+
+
+
+
--- web-server-SBC/startup.lisp
+++ web-server-SBC/startup.lisp
@@ -75,66 +75,10 @@
 	      (:td (:h2 (:a :href (format nil "~a" (tool-link x))
 			    (fmt "link"))))))))))
 
-;;;; file-path color-set ip-set
-(add-tool "pcap-process")
 
-(define-url-fn (pcap-process)
-  (standard-page-with-title (:title "Pcap-process")
-    (:h1 (:u "Pcap Process Settings"))
-    (:hr :size 5 :color "orange")
-    (:br)
-    (:form :action "/pp-run.htm" :method "post"
-	   :onsubmit (ps-inline
-		      (when (= ppfilepath.value "")
-			(alert "File path empty!")
-			(return false)))
-	   (:table
-	    (:tr
-	     (:td :valign "top" "1. File path:")
-	     (:td :colspan 3
-		  (:input :type "text" :name "ppfilepath"
-			  :size 111 :class "txt")))
-	    (:tr
-	     (:td :valign "top" "2. IP picking:")
-	     (:td :align "right" "Internal IPs &#8656")
-	     (:td "&#8658 SBC Internal IPs"))
-	    (:tr
-	     (:td)
-	     (:td  (:textarea :rows 4 :cols 40 :name "ppip1"))
-	     (:td  (:textarea :rows 4 :cols 40 :name "ppip2"))
-	     (:td ))
-	    (:tr
-	     (:td) (:td)
-	     (:td :align "right" "SBC External IPs &#8656")
-	     (:td "&#8658 External IPs"))
-	    (:tr
-	     (:td)(:td)
-	     (:td  (:textarea :rows 4 :cols 40 :name "ppip3"))
-	     (:td  (:textarea :rows 4 :cols 40 :name "ppip4")))
-	    (:tr
-	     (:td :valign "top" "3. Text color:")
-	     (:td :colspan 2
-		  (:textarea :rows 15 :cols 60
-			     :name "colorsettings" :class "txt")))
-	    (:tr
-	     (:td)
-	     (:td  (:input :type "reset" :value "Reset" :class "btn"))
-	     (:td  (:input :type "submit" :value "Process" :class "btn")))))))
+(print "XXXXXXXXXXXXXXXXXXXXXXXXXX")
+(print "XXXXXXXXXXXXXXXXXXXXXXXXXX")
+(print "       CNET primer")
+(print "XXXXXXXXXXXXXXXXXXXXXXXXXX")
+(print "XXXXXXXXXXXXXXXXXXXXXXXXXX")
 
-(define-url-fn (pp-run)
-  (let ((filepath (parameter "ppfilepath"))
-	(ip1 (parameter "ppip1"))
-	(ip2 (parameter "ppip2"))
-	(ip3 (parameter "ppip3"))
-	(ip4 (parameter "ppip4"))
-	(colorset (parameter "colorsettings")))
-    ;;;; print debug didn't output anything in slime-repl, use log-message
-    ;;(print ip1)
-    ;;(debug "pp-run" filepath ip1 ip2 ip3 ip4 colorset)
-    (hunchentoot:log-message* 3 "~a~%" filepath)
-    (standard-page-with-title (:title "pcap analysis")
-      (htm
-	(fmt "~a" (output2html-string filepath))))))
-
-
-;;(output2html-string "c:/Users/Administrator/Desktop/program/little-things/pcap-output-for-sip-check/123.pcapng")
